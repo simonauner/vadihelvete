4acc33bad3d80cf0deba671d8f492072d23c954a
Simon Aunér <simonauner@gmail.com>
2022-01-18 21:25:02 +0100

    Add meta titles

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,14 +14,31 @@ const Home: NextPage<HomePageProps> = ({ suggestion }) => {
     location.reload();
   };
 
+  const metaTitle = "Vad i helevete ska jag göra nu?";
+  const metaDescription = "Vet du inte riktigt vad du ska göra med ditt liv?";
+  const metaUrl = "https://vadihelvete.se";
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Vad i helvete ska jag göra nu?</title>
-        <meta
-          name="description"
-          content="Vet du inte riktigt vad du ska göra?"
-        />
+        <title>{metaTitle}</title>
+        <meta name="description" content={metaDescription} />
+        {/* facebook open graph tags */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={metaUrl} />
+        <meta property="og:title" content={metaTitle} />
+        <meta property="og:description" content={metaDescription} />
+
+        {/* twitter */}
+        <meta name="twitter:domain" content="vadihelvete.se" />
+        <meta name="twitter:title" content={metaTitle} />
+        <meta name="twitter:description" content={metaDescription} />
+        <meta name="twitter:url" content={metaUrl} />
+        <meta name="twitter:label1" content="Slut på idéer?" />
+        <meta name="twitter:data1" content="Ja! ✅" />
+        <meta name="twitter:label2" content="Tycker om att få order?" />
+        <meta name="twitter:data2" content="Oh, yes! ✅" />
+
         <link
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎯</text></svg>"
@@ -39,11 +56,7 @@ const Home: NextPage<HomePageProps> = ({ suggestion }) => {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="https://vercel.com" target="_blank" rel="noopener noreferrer">
           Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
@@ -57,7 +70,6 @@ const Home: NextPage<HomePageProps> = ({ suggestion }) => {
 export async function getServerSideProps() {
   const suggestion = getSuggestion();
 
-  // Pass data to the page via props
   return { props: { suggestion } };
 }
 
