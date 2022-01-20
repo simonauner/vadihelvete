53be199a6a82b07214e3ced87cd70f6cf3b5ce2f
Simon Aunér <simonauner@gmail.com>
2022-01-20 21:40:50 +0100

    Use preact

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Thanks https://darrenwhite.dev/blog/nextjs-replace-react-with-preact
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+
+    return config;
+  },
 }
 
 module.exports = nextConfig
