dadbba264a4f38b0d0d2ca8c5ae20a32f3174acf
Simon Aunér <simonauner@gmail.com>
2022-01-20 20:44:42 +0100

    Add more suggestions

--- src/Suggestions.ts
+++ src/Suggestions.ts
@@ -8,6 +8,12 @@ export const getSuggestion = (): string => {
     "vika tvätt",
     "hänga upp tvätt",
     "göra rent i badrummet",
+    "laga middag",
+    "göra rent kattlådan",
+    "gå ut med soporna",
+    "dammsuga",
+    "göra rent i köket",
+    "åka och handla mat",
   ];
 
   return suggestions[Math.round(Math.random() * (suggestions.length - 1))];
